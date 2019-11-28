845f6176e70632dadb5a085c3277b1c83d9a3ed1
agnaev <agnev@mwpost.ru>
2019-11-28 21:36:57 +0300

    запись в файл исходного алфавита и соответствующих им символов

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.decoded_textBox = new System.Windows.Forms.TextBox();
             this.encoded_textBox = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.IsEncoding = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // decoded_textBox
@@ -38,7 +38,7 @@
             this.decoded_textBox.Location = new System.Drawing.Point(12, 40);
             this.decoded_textBox.Multiline = true;
             this.decoded_textBox.Name = "decoded_textBox";
-            this.decoded_textBox.Size = new System.Drawing.Size(367, 398);
+            this.decoded_textBox.Size = new System.Drawing.Size(367, 338);
             this.decoded_textBox.TabIndex = 0;
             this.decoded_textBox.TextChanged += new System.EventHandler(this.Decoded_TextChanged);
             // 
@@ -48,28 +48,28 @@
             this.encoded_textBox.Multiline = true;
             this.encoded_textBox.Name = "encoded_textBox";
             this.encoded_textBox.ReadOnly = true;
-            this.encoded_textBox.Size = new System.Drawing.Size(403, 398);
+            this.encoded_textBox.Size = new System.Drawing.Size(403, 338);
             this.encoded_textBox.TabIndex = 1;
             // 
-            // checkBox1
+            // IsEncoding
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Checked = true;
-            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox1.Location = new System.Drawing.Point(12, 12);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(71, 17);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "Encoding";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            this.IsEncoding.AutoSize = true;
+            this.IsEncoding.Checked = true;
+            this.IsEncoding.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.IsEncoding.Location = new System.Drawing.Point(12, 12);
+            this.IsEncoding.Name = "IsEncoding";
+            this.IsEncoding.Size = new System.Drawing.Size(71, 17);
+            this.IsEncoding.TabIndex = 2;
+            this.IsEncoding.Text = "Encoding";
+            this.IsEncoding.UseVisualStyleBackColor = true;
+            this.IsEncoding.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.IsEncoding);
             this.Controls.Add(this.encoded_textBox);
             this.Controls.Add(this.decoded_textBox);
             this.Name = "Form1";
@@ -83,7 +83,7 @@
 
         private System.Windows.Forms.TextBox decoded_textBox;
         private System.Windows.Forms.TextBox encoded_textBox;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox IsEncoding;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,32 +13,52 @@ namespace ReplacementCipher
 {
     public partial class Form1 : Form
     {
-        List<char> Characters = new List<char>();
-        List<char> MixedCharacters = new List<char>();
+        readonly List<char> Characters = new List<char>();
+        readonly List<char> MixedCharacters = new List<char>();
         public Form1()
         {
             InitializeComponent();
-            for(int i = 0; i < 256; i++)
+            for(char i = 'а'; i <= 'я'; i++)
             {
-                Characters.Add((char)i);
+                Characters.Add(i);
             }
+            Characters.Add(' ');
             SubtractiveGenerator r = new SubtractiveGenerator(10);
             MixedCharacters = Characters.OrderBy(x => r.Next()).ToList();
+            string path = Directory.GetCurrentDirectory() + "\\Characters.txt";
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                for (int i = 0; i < Characters.Count(); i++)
+                {
+                    writer.WriteLine($"{Characters[i]} {MixedCharacters[i]}");
+                }
+            }
+            System.Diagnostics.Process.Start(path);
         }
 
         private void Decoded_TextChanged(object sender, EventArgs e)
         {
-            encoded_textBox.Text = string.Empty;
-            for (int i = 0; i < decoded_textBox.Text.Length; i++)
-            { 
-                if (checkBox1.Checked)
+            try
+            {
+                encoded_textBox.Text = string.Empty;
+                for (int i = 0; i < decoded_textBox.Text.Length; i++)
                 {
-                    encoded_textBox.Text += MixedCharacters[Characters.IndexOf(decoded_textBox.Text[i])];
+                    if (IsEncoding.Checked)
+                    {
+                        if (Characters.Contains(decoded_textBox.Text[i]))
+                        {
+                            encoded_textBox.Text += MixedCharacters[Characters.IndexOf(decoded_textBox.Text[i])];
+                        }
+                    }
+                    else
+                    {
+                        encoded_textBox.Text += Characters[MixedCharacters.IndexOf(decoded_textBox.Text[i])];
+                    }
                 }
-                else
-                {
-                    encoded_textBox.Text += Characters[MixedCharacters.IndexOf(decoded_textBox.Text[i])];
-                }
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message + exc.InnerException?.InnerException?.Message);
             }
         }
 
